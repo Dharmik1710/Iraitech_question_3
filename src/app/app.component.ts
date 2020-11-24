e11297d6d14f5c32df9288b8c523b80c4a6f9b32
Dharmik Patel <dharmik1289@gmail.com>
2020-11-25 02:06:30 +0530

    ques3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { strict } from 'assert';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'ques3';
+  
+  contactForm: FormGroup;
+  delButtonEnabled: Boolean = false;
+
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
+  
+  createForm(){
+    this.contactForm = this.fb.group({
+      phoneNumbers: this.fb.array([
+        this.fb.control('')
+      ])
+    })
+  }
+
+  get phoneNumbers(){
+    return this.contactForm.get('phoneNumbers') as FormArray;
+  }
+
+  addNumber(){
+    this.delButtonEnabled = true;
+    this.phoneNumbers.push(this.fb.control(''));
+  }
+
+  removeNumber(index: number){
+    this.phoneNumbers.removeAt(this.phoneNumbers.value.indexOf(index));
+    if(this.phoneNumbers.length==1){
+      this.delButtonEnabled = false;
+      return;
+    }
+  }
+
+  onSubmit(){
+    let array = [];
+    for(let i=0; i< this.phoneNumbers.length; i++){
+      array.push({ ['PhoneNumber'+i]: this.phoneNumbers.value[i]});
+    }
+    console.log(array);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
